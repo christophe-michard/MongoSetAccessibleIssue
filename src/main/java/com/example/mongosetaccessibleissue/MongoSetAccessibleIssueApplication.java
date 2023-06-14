0e1c2ad5d04f3cb36197a39eb812222a66728ffa
Christophe Michard <christophe.michard@rakuten.com>
2023-06-14 14:32:28 +0900

    Initialize a new Spring Boot project

---
+++ src/main/java/com/example/mongosetaccessibleissue/MongoSetAccessibleIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongosetaccessibleissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSetAccessibleIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoSetAccessibleIssueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongosetaccessibleissue/MongoSetAccessibleIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongosetaccessibleissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSetAccessibleIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
